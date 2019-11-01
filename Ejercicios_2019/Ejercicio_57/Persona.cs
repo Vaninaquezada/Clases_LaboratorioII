ae3a8a036fddb7e748c1f82337d93c28e2ec249c
alumno <alumno@utnfra.com.ar>
2019-10-31 21:45:55 -0300

    clase 31-10

---
+++ Ejercicios_2019/Ejercicio_57/Persona.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+
+
+namespace Ejercicio_57
+{
+   public class Persona
+
+        
+    {
+        private string nombre;
+        private string apellido;
+
+        public Persona(string nombre,string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+
+        }
+
+        public static bool Guardar(Persona persona)
+        {
+            try
+            {
+                XmlTextWriter writer = new XmlTextWriter("/archivo.txt", System.Text.Encoding.UTF8);
+
+                XmlSerializer ser = new XmlSerializer(typeof(Persona));
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+               
+            }
+
+             return false;
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Nombre: {0}", this.nombre);
+            sb.AppendFormat("Apellido: {0}", this.apellido);
+            return sb.ToString();
+        }
+    }
+}
