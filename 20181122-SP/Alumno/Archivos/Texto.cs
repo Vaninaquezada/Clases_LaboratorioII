a5e990372d1b6a38905962d98deb6734cd2ef8da
alumno <alumno@utnfra.com.ar>
2019-11-14 21:15:57 -0300

    ejemplo parcial2

---
+++ 20181122-SP/Alumno/Archivos/Texto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidades;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<Queue<Patente>>
+    {
+        public void Guardar(string archivo, Queue<Patente> datos)
+        {
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(AppDomain.CurrentDomain.BaseDirectory + archivo + ".txt", true))
+                {
+                    file.WriteLine(datos);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void Leer(string archivo, out Queue<Patente> datos)
+        {
+            try
+            {
+                using (System.IO.StreamReader file = new System.IO.StreamReader(AppDomain.CurrentDomain.BaseDirectory + archivo + ".txt", true))
+                {
+                    String line;
+
+                    datos = new Queue<Patente>();
+
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        
+                        datos.Enqueue(line.ValidarPatente());
+                    }
+                }
+
+               
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+     
+    }
+}
---
+++ 20181122-SP/Alumno/Archivos/Xml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Archivos
+{
+    public class Xml<T> : IArchivo<T>
+    {
+        public void Guardar(string archivo, T datos)
+        {
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                TextWriter writer = new StreamWriter(archivo);
+                serializer.Serialize(writer, datos);
+                writer.Close();
+
+               
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void Leer(string archivo, out T datos)
+        {
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                TextReader reader = new StreamReader(archivo);
+                datos = (T)serializer.Deserialize(reader);
+                reader.Close();
+
+                
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
